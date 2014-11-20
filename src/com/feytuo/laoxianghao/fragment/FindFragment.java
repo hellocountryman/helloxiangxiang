70919a146c908cfc094c384826589dfb5d704625
tangxiaopeng <tangpengtp007@126.com>
2014-11-20 14:21:56 +0800

    peng11.20.14.21

--- src/com/feytuo/laoxianghao/fragment/FindFragment.java
+++ src/com/feytuo/laoxianghao/fragment/FindFragment.java
@@ -1,17 +1,22 @@
 package com.feytuo.laoxianghao.fragment;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
-import android.widget.LinearLayout;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
 import com.feytuo.laoxianghao.MessageCellectActivity;
 import com.feytuo.laoxianghao.R;
+import com.feytuo.laoxianghao.global.HeadImageChoose;
+import com.feytuo.laoxianghao.util.NetUtil;
 
 public class FindFragment extends Fragment {
 
@@ -19,15 +24,16 @@ public class FindFragment extends Fragment {
 	private RelativeLayout findDuanziRelac;
 	private RelativeLayout findktvRelac;
 	private RelativeLayout findtShowRelac;
+	private ImageButton indexTopicImg;//
+	private ImageButton indeDuanziImg;
+	private ImageButton indexKtvImg;//
+	private ImageButton indexShowImg;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-
 		View view = inflater.inflate(R.layout.find_activity, container, false);
-
 		return view;
-
 	}
 
 	@Override
@@ -38,6 +44,18 @@ public class FindFragment extends Fragment {
 	}
 
 	public void initview() {
+		indexTopicImg = (ImageButton) getActivity().findViewById(
+				R.id.find_topic_img);
+		indeDuanziImg = (ImageButton) getActivity().findViewById(
+				R.id.find_duanzi_img);
+		indexKtvImg = (ImageButton) getActivity().findViewById(
+				R.id.find_ktv_img);
+		indexShowImg = (ImageButton) getActivity().findViewById(
+				R.id.find_show_img);
+		NetUtil.corner(getActivity(),R.drawable.findtopic, indexTopicImg);//设置圆角
+		NetUtil.corner(getActivity(),R.drawable.findduanzi, indeDuanziImg);
+		NetUtil.corner(getActivity(),R.drawable.findktv, indexKtvImg);
+		NetUtil.corner(getActivity(),R.drawable.findshow, indexShowImg);
 
 		findTopicRelac = (RelativeLayout) getActivity().findViewById(
 				R.id.find_topic_linear);
@@ -78,4 +96,6 @@ public class FindFragment extends Fragment {
 			}
 		}
 	};
+
+
 }
